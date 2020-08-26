d5a55a2faedd9ccb003e2ba790fde452d977825e
Denze1l <dvornikovd.o.1998@gmail.com>
2020-08-26 16:46:34 +0300

    add video with timechange buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Player, ControlBar } from "video-react";
+import styles from "./app.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      source: "https://media.w3.org/2010/05/sintel/trailer_hd.mp4",
+    };
+    this.changeCurrentTime = this.changeCurrentTime.bind(this);
+  }
+  componentDidMount() {
+    // subscribe state change
+    this.player.subscribeToStateChange(this.handleStateChange.bind(this));
+  }
+  handleStateChange(state) {
+    // copy player state to this component's state
+    this.setState({
+      player: state,
+    });
+  }
+
+  // also you can choose rewind time gap by your self
+  changeCurrentTime(seconds) {
+    return () => {
+      const { player } = this.player.getState();
+      if (player.paused === false) {
+        this.player.seek(player.currentTime + seconds);
+      }
+    };
+  }
+
+  render() {
+    const { source } = this.state;
+    return (
+      <>
+        <div className={styles.videoDiv}>
+          <Player
+            playsInline
+            ref={(player) => {
+              this.player = player;
+            }}
+          >
+            <source src={source} />
+            <ControlBar autoHide={true} />
+          </Player>
+        </div>
+        <div className={styles.buttonsDiv}>
+          <button onClick={this.changeCurrentTime(-10)}>Time -10s</button>
+          <button onClick={this.changeCurrentTime(10)}>Time +10s</button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
